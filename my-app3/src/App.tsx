a9675c98260ac65d4a0a9ffa6590c2388e93acdf
Faizan ul Haq <123570147+FaizanUlHaq262@users.noreply.github.com>
2025-03-10 01:07:44 +0500

    uncommented the protectedroutes from the app.tsx to make sure that the user is authenticated before accessing the protected routes and logout button now redirects user to the login page if logged out from any page

--- my-app3/src/App.tsx
+++ my-app3/src/App.tsx
@@ -23,25 +23,25 @@ function App() {
           <Route
             path="/zoom-integration"
             element={
-              // <ProtectedRoute>
+              <ProtectedRoute>
                 <ZoomIntegrationPage />
-              // </ProtectedRoute>
+              </ProtectedRoute>
             }
           />
           <Route
             path="/dashboard"
             element={
-              // <ProtectedRoute>
+              <ProtectedRoute>
                 <DashboardPage />
-              // </ProtectedRoute>
+              </ProtectedRoute>
             }
           />
           <Route
             path="/create-call"
             element={
-              // <ProtectedRoute>
+              <ProtectedRoute>
                 <CreateCallPage />
-              // </ProtectedRoute>
+              </ProtectedRoute>
             }
           />
           <Route path="*" element={<Navigate to="/" replace />} />
